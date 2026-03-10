928d7a787318228f5d4ff55a19afe685c1ded44e
gooberlemonade <104174925+lemonade-js@users.noreply.github.com>
2026-03-09 23:30:09 -0600

    Refactor custom sounds and music configuration

--- config.lua
+++ config.lua
@@ -37,152 +37,250 @@ local function compareTable(a, b)
 end
 
 if not mod.sConfigState then
-    mod.sConfigState = copyTable(mod.config.music)
+    mod.sConfigState = copyTable(mod.config.customsounds.music)
 end
 
-if imgui.Button("Open CS Folder") then
-    local path = os.getenv("APPDATA") .. "\\beatblock\\Mods\\customsounds\\customsounds"
-    os.execute('start "" "' .. path .. '"')
-end
-
-imgui.NewLine()
-imgui.Separator()
-imgui.NewLine()
-
-imgui.SetWindowFontScale(2)
-imgui.Text("SFX Replacements")
-imgui.SetWindowFontScale(1)
-
-local customsounds = {
-    sfx = {
-        { label = "Barely", value = "barely", fallback = "assets/sfx/barely.ogg" },
-        { label = "Block", value = "block", fallback = "assets/sfx/click.ogg" },
-        { label = "Click", value = "click", fallback = "assets/sfx/click.ogg" },
-        { label = "Hold", value = "hold", fallback = "assets/sfx/hold.ogg" },
-        { label = "Hover", value = "hover", fallback = "assets/sfx/click.ogg" },
-        { label = "Mine", value = "mine", fallback = "assets/sfx/mine.ogg" },
-        { label = "Pause", value = "pause", fallback = "assets/sfx/pause.ogg" },
-        { label = "Side", value = "side", fallback = "assets/sfx/click.ogg" },
-        { label = "Tap", value = "tap", fallback = "assets/sfx/tap.ogg" },
-        { label = "Miss", value = "miss", fallback = "assets/sfx/mine.ogg" }
-    },
-    music = {
-        { label = "Menu Loop", value = "menuloop", fallback = "assets/music/menuloop.ogg" },
-        { label = "Caution", value = "caution", fallback = "assets/music/caution.ogg" }
-    }
-}
-
--- local audioFiles = {sfx = {}, music = {}}
-
-local audioFileExtensions = {"mp3", "ogg", "wav", "aac", "flac", "m4a", "wma", "opus"} -- no idea if all of these work, but whatevs
-
-local function isAudio(fileName)
-    local ext = fileName:match("^.+%.(.+)$") -- get text after last "."
-    if not ext then return false end
-    ext = ext:lower()
-    for _, validExt in ipairs(audioFileExtensions) do
-        if ext == validExt then
-            return true
-        end
-    end
-    return false
-end
-
-if imgui.BeginTabBar("sfxconfig") then
-    for i = 1, #customsounds.sfx, 1 do    
-        if imgui.BeginTabItem(customsounds.sfx[i].label .. "##sfxconfig") then
-    
-            local sounds = love.filesystem.getDirectoryItems("Mods/customsounds/customsounds/sfx/" .. customsounds.sfx[i].value)
-
-            imgui.Text("Selected: " .. mod.config.sfx[customsounds.sfx[i].value]);
-
-            if imgui.Button("Default##" .. customsounds.sfx[i].value) then
-                print("Reset " .. customsounds.sfx[i].value .. " to default")
-
-                mod.config.sfx[customsounds.sfx[i].value] = "default"
-
-                sm:replaceSound(customsounds.sfx[i].value, customsounds.sfx[i].fallback) 
-            end
-
-            imgui.NewLine()
-
-            for _, sound in ipairs(sounds) do
-                if isAudio(sound) then
-                    if imgui.Button(sound .. "##" .. customsounds.sfx[i].value) then
-                        print(sound)
-                    
-                        mod.config.sfx[customsounds.sfx[i].value] = sound
-                    
-                        sm:replaceSound(customsounds.sfx[i].value, "Mods/customsounds/customsounds/sfx/" .. customsounds.sfx[i].value .. "/" .. sound)
-
-                        -- SFX previews!!!!!!!!
-                        te.playOne("Mods/customsounds/customsounds/sfx/" .. customsounds.sfx[i].value .. "/" .. sound, "static", "sfx")
-                    end
-                end
-            end
-
-            imgui.EndTabItem()
-        end
-    end
-
-    imgui.EndTabBar()
-end
-
-imgui.NewLine()
-imgui.NewLine()
-imgui.NewLine()
-
-imgui.SetWindowFontScale(2)
-imgui.Text("Music Replacements")
-imgui.SetWindowFontScale(1)
-
-if imgui.BeginTabBar("mconfig") then
-    for i = 1, #customsounds.music, 1 do
-        if imgui.BeginTabItem(customsounds.music[i].label .. "##mconfig") then
-    
-            local sounds = love.filesystem.getDirectoryItems("Mods/customsounds/customsounds/music/" .. customsounds.music[i].value)
-
-            imgui.Text("Selected: " .. mod.config.music[customsounds.music[i].value]);
-
-            if imgui.Button("Default##" .. customsounds.music[i].value) then
-                print("Reset " .. customsounds.music[i].value .. " to default")
-
-                mod.config.music[customsounds.music[i].value] = "default"
-            end
-
-            imgui.NewLine()
-
-            for _, sound in ipairs(sounds) do
-                if isAudio(sound) then
-                    if imgui.Button(sound .. "##" .. customsounds.music[i].value) then
-                        print(sound)
-                    
-                        mod.config.music[customsounds.music[i].value] = sound
-                    
-                        --sounds:replaceSound(customsounds.music[i].value, "Mods/customsounds/customsounds/music/" .. customsounds.music[i].value .. "/" .. sound)
-                    end
-                end
-            end
-
-            if customsounds.music[i].value == "menuloop" then
-                imgui.NewLine()
-
-                mod.config.music.menuloopBPM = helpers.InputInt("BPM (default: 108)", mod.config.music.menuloopBPM)
-                mod.config.music.menuloopOffset = helpers.InputInt("Offset (ms)(default: 0)", mod.config.music.menuloopOffset)
-            end
-
-            imgui.EndTabItem()
-        end
-    end
-    imgui.EndTabBar()
-end
-
-imgui.NewLine()
-imgui.Separator()
-imgui.NewLine()
-
-if not compareTable(mod.sConfigState, mod.config.music) then 
-    imgui.TextColored({1, 0.35, 0, 1}, "MUSIC CHANGES WERE MADE. RESTART REQUIRED AFTER SAVING.")
+if imgui.TreeNode_Str("Chart Progress") then
+    imgui.SetWindowFontScale(2)
+    imgui.Text("Chart Progress")
+    imgui.SetWindowFontScale(1)
+    imgui.Text("Adds bars to the extended UI showing level progress")
 
     imgui.NewLine()
+    imgui.Separator()
+    imgui.NewLine()
+
+    imgui.SetWindowFontScale(2)
+    imgui.Text("Bar Positions")
+    imgui.SetWindowFontScale(1)
+
+    mod.config.chartprog.topBar = helpers.InputBool("Enable top bar", mod.config.chartprog.topBar)
+    mod.config.chartprog.bottomBar = helpers.InputBool("Enable bottom bar", mod.config.chartprog.bottomBar)
+
+    imgui.NewLine()
+    imgui.Separator()
+    imgui.NewLine()
+
+    imgui.SetWindowFontScale(2)
+    imgui.Text("Bar Visuals")
+    imgui.SetWindowFontScale(1)
+
+    mod.config.chartprog.useVFXcolor = helpers.InputBool("Use UI color", mod.config.chartprog.useVFXcolor)
+    helpers.imguiHelpMarker("Use the UI color rather than the player colors")
+
+    mod.config.chartprog.centerBar = helpers.InputBool("Center bars", mod.config.chartprog.centerBar)
+    helpers.imguiHelpMarker("Centers the bars horizontally")
+
+    mod.config.chartprog.reverseDirection = helpers.InputBool("Reverse bar direction", mod.config.chartprog.reverseDirection)
+    helpers.imguiHelpMarker("Makes the bars shrink instead of grow")
+
+    imgui.NewLine()
+
+    mod.config.chartprog.showPercentage = helpers.InputBool("Show Percentage", mod.config.chartprog.showPercentage)
+    mod.config.chartprog.showDecimalPercentage = helpers.InputBool("Show Decimal Percentage", mod.config.chartprog.showDecimalPercentage)
+
+    imgui.NewLine()
+
+    imgui.SetNextItemWidth(120)
+    mod.config.chartprog.barHeight = helpers.InputInt("Bar thickness", mod.config.chartprog.barHeight)
+    helpers.imguiHelpMarker("How tall the bars are")
+    
+    imgui.NewLine()
+    imgui.Separator()
+    imgui.NewLine()
+
+    imgui.TreePop()
+end
+
+if imgui.TreeNode_Str("Custom Sounds") then
+    imgui.SetWindowFontScale(2)
+    imgui.Text("Custom Sounds")
+    imgui.SetWindowFontScale(1)
+    imgui.Text("Allows for replacing sound effects and music in game")
+
+    imgui.NewLine()
+    imgui.Separator()
+    imgui.NewLine()
+
+    if imgui.Button("Open CS Folder") then
+        local path = os.getenv("APPDATA") .. "\\beatblock\\Mods\\lemontweaks\\customsounds"
+        os.execute('start "" "' .. path .. '"')
+    end
+
+    imgui.NewLine()
+    imgui.Separator()
+    imgui.NewLine()
+
+    local customsounds = {
+        sfx = {
+            { label = "Barely", value = "barely", fallback = "assets/sfx/barely.ogg" },
+            { label = "Block", value = "block", fallback = "assets/sfx/click.ogg" },
+            { label = "Click", value = "click", fallback = "assets/sfx/click.ogg" },
+            { label = "Hold", value = "hold", fallback = "assets/sfx/hold.ogg" },
+            { label = "Hover", value = "hover", fallback = "assets/sfx/click.ogg" },
+            { label = "Mine", value = "mine", fallback = "assets/sfx/mine.ogg" },
+            { label = "Pause", value = "pause", fallback = "assets/sfx/pause.ogg" },
+            { label = "Side", value = "side", fallback = "assets/sfx/click.ogg" },
+            { label = "Tap", value = "tap", fallback = "assets/sfx/tap.ogg" },
+            { label = "Miss", value = "miss", fallback = "assets/sfx/mine.ogg" }
+        },
+        music = {
+            { label = "Menu Loop", value = "menuloop", fallback = "assets/music/menuloop.ogg" },
+            { label = "Caution", value = "caution", fallback = "assets/music/caution.ogg" }
+        }
+    }
+
+    -- local audioFiles = {sfx = {}, music = {}}
+
+    local audioFileExtensions = {"mp3", "ogg", "wav", "aac", "flac", "m4a", "wma", "opus"} -- no idea if all of these work, but whatevs
+
+    local function isAudio(fileName)
+        local ext = fileName:match("^.+%.(.+)$") -- get text after last "."
+        if not ext then return false end
+        ext = ext:lower()
+        for _, validExt in ipairs(audioFileExtensions) do
+            if ext == validExt then
+                return true
+            end
+        end
+        return false
+    end
+
+    if imgui.BeginTabBar("sfxconfig") then
+        for i = 1, #customsounds.sfx, 1 do    
+            if imgui.BeginTabItem(customsounds.sfx[i].label .. "##sfxconfig") then
+        
+                local sounds = love.filesystem.getDirectoryItems("Mods/lemontweaks/customsounds/sfx/" .. customsounds.sfx[i].value)
+
+                imgui.Text("Selected: " .. mod.config.customsounds.sfx[customsounds.sfx[i].value]);
+
+                if imgui.Button("Default##" .. customsounds.sfx[i].value) then
+                    print("[lemontweaks] Reset " .. customsounds.sfx[i].value .. " to default")
+
+                    mod.config.customsounds.sfx[customsounds.sfx[i].value] = "default"
+
+                    sm:replaceSound(customsounds.sfx[i].value, customsounds.sfx[i].fallback) 
+                end
+
+                imgui.NewLine()
+
+                for _, sound in ipairs(sounds) do
+                    if isAudio(sound) then
+                        if imgui.Button(sound .. "##" .. customsounds.sfx[i].value) then
+                            print("[lemontweaks] Set " .. customsounds.sfx[i].value .. " to " .. sound)
+                        
+                            mod.config.customsounds.sfx[customsounds.sfx[i].value] = sound
+                        
+                            sm:replaceSound(customsounds.sfx[i].value, "Mods/lemontweaks/customsounds/sfx/" .. customsounds.sfx[i].value .. "/" .. sound)
+
+                            -- SFX previews!!!!!!!!
+                            print("[lemontweaks] playing " .. sound)
+                            te.playOne("Mods/lemontweaks/customsounds/sfx/" .. customsounds.sfx[i].value .. "/" .. sound, "static", "sfx")
+                        end
+                    end
+                end
+
+                imgui.EndTabItem()
+            end
+        end
+
+        imgui.EndTabBar()
+    end
+
+    imgui.NewLine()
+    imgui.NewLine()
+    imgui.NewLine()
+
+    imgui.SetWindowFontScale(2)
+    imgui.Text("Music Replacements")
+    imgui.SetWindowFontScale(1)
+
+    if imgui.BeginTabBar("mconfig") then
+        for i = 1, #customsounds.music, 1 do
+            if imgui.BeginTabItem(customsounds.music[i].label .. "##mconfig") then
+        
+                local sounds = love.filesystem.getDirectoryItems("Mods/lemontweaks/customsounds/music/" .. customsounds.music[i].value)
+
+                imgui.Text("Selected: " .. mod.config.customsounds.music[customsounds.music[i].value]);
+
+                if imgui.Button("Default##" .. customsounds.music[i].value) then
+                    print("[lemontweaks] Reset " .. customsounds.music[i].value .. " to default")
+
+                    mod.config.customsounds.music[customsounds.music[i].value] = "default"
+                end
+
+                imgui.NewLine()
+
+                for _, sound in ipairs(sounds) do
+                    if isAudio(sound) then
+                        if imgui.Button(sound .. "##" .. customsounds.music[i].value) then
+                            print("[lemontweaks] Set " .. customsounds.music[i].value .. " to " .. sound)
+                        
+                            mod.config.customsounds.music[customsounds.music[i].value] = sound
+                        
+                            --sounds:replaceSound(customsounds.music[i].value, "Mods/lemontweaks/customsounds/music/" .. customsounds.music[i].value .. "/" .. sound)
+                        end
+                    end
+                end
+
+                if customsounds.music[i].value == "menuloop" then
+                    imgui.NewLine()
+
+                    mod.config.customsounds.music.useCustomPlayer = helpers.InputBool("Use Custom Player", mod.config.customsounds.music.useCustomPlayer)
+                    helpers.imguiHelpMarker("Randomly shuffles songs from installed maps, no need to pick your favorite")
+                    
+                    imgui.NewLine()
+
+                    if (mod.config.customsounds.music.useCustomPlayer) then
+                        imgui.BeginDisabled();
+                    end
+
+                    imgui.SetNextItemWidth(120)
+                    mod.config.customsounds.music.menuloopBPM = helpers.InputInt("BPM (default: 108)", mod.config.customsounds.music.menuloopBPM)
+
+                    imgui.SetNextItemWidth(120)
+                    mod.config.customsounds.music.menuloopOffset = helpers.InputInt("Offset (ms)(default: 0)", mod.config.customsounds.music.menuloopOffset)
+
+                    if (mod.config.customsounds.music.useCustomPlayer) then
+                        imgui.EndDisabled();
+                    end
+                end
+
+                imgui.EndTabItem()
+            end
+        end
+        imgui.EndTabBar()
+    end
+
+    imgui.NewLine()
+    imgui.Separator()
+    imgui.NewLine()
+
+    if not compareTable(mod.sConfigState, mod.config.customsounds.music) then 
+        imgui.TextColored({1, 0.35, 0, 1}, "MUSIC CHANGES WERE MADE. RESTART REQUIRED AFTER SAVING.")
+
+        imgui.NewLine()
+    end
+
+    imgui.TreePop()
+end
+
+if imgui.TreeNode_Str("Miscellaneous") then
+    imgui.SetWindowFontScale(2)
+    imgui.Text("Miscellaneous")
+    imgui.SetWindowFontScale(1)
+    imgui.Text("Some random freatures without enough options for its own category")
+
+    imgui.NewLine()
+    imgui.Separator()
+    imgui.NewLine()
+
+    mod.config.misc.customwiplevels = helpers.InputBool("Custom WIP Levels Tab", mod.config.misc.customwiplevels)
+    mod.config.misc.nodiscord = helpers.InputBool("Remove Discord Tab", mod.config.misc.nodiscord)
+    
+    imgui.NewLine()
+    imgui.Separator()
+    imgui.NewLine()
+
+    imgui.TreePop()
 end
